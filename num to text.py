435d26d456d3c3483a05f24ecbd38792392a880a
Ehsan <57656112+iehsansh@users.noreply.github.com>
2021-02-02 13:11:15 +0300

    Add files via upload
    Convert phone number to text. User will input phone number and output is this number in text.

---
+++ num to text.py
@@ -0,0 +1,6 @@
+digits = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
+x = input()
+for i in x:
+    phone = int(i)
+    print(digits[phone])
+msss = input("Enter to exit: ")
